0524d5e58ec4ecbb2c510269c953e7d3cdc1bd7e
Anthony Gubler <agubler@sitepen.com>
2017-03-16 17:53:47 +0000

    demo-5

---
+++ src/widgets/createHelloWorldElement.ts
@@ -0,0 +1,21 @@
+import { CustomElementDescriptor } from '@dojo/widget-core/customElements';
+import HelloWorld from './HelloWorld';
+
+export default function createDialogElement(): CustomElementDescriptor {
+	return {
+		tagName: 'dojo-hello-world',
+		widgetConstructor: HelloWorld,
+		attributes: [
+			{
+				attributeName: 'stranger',
+				value: value => value === 'false' || value === '0' ? false : true
+			}
+		],
+		events: [
+			{
+				propertyName: 'toggleStranger',
+				eventName: 'toggleStranger'
+			}
+		]
+	};
+};
