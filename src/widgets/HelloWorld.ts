a382d56fb83114e4a52e0c85c035f7a22bbcd230
Anthony Gubler <agubler@sitepen.com>
2017-03-16 17:27:27 +0000

    demo-4

--- src/widgets/HelloWorld.ts
+++ src/widgets/HelloWorld.ts
@@ -7,35 +7,24 @@ import * as css from './styles/HelloWorld.css';
 
 export interface HelloWorldProperties extends WidgetProperties {
 	stranger: boolean;
-<<<<<<< e2c9346a8206c7f49b5f9a7b0e6c00951d5a3eec
 	toggleStranger: Function;
-=======
->>>>>>> demo-3
 }
 
 export const HelloWorldBase = ThemeableMixin(WidgetBase);
 
 @theme(css)
 export default class HelloWorld extends HelloWorldBase<HelloWorldProperties> {
-<<<<<<< e2c9346a8206c7f49b5f9a7b0e6c00951d5a3eec
 
 	private _onClick() {
 		this.properties.toggleStranger && this.properties.toggleStranger();
 	}
 
 	protected render(): DNode {
-=======
-	render() {
->>>>>>> demo-3
 		const classes = this.classes(
 			css.hello,
 			this.properties.stranger ? css.upsidedown : null
 		);
 
-<<<<<<< e2c9346a8206c7f49b5f9a7b0e6c00951d5a3eec
 		return v('div', { classes, onclick: this._onClick }, [ 'Hello, Dojo World!' ]);
-=======
-		return v('div', { classes }, [ 'Hello, Dojo World!' ]);
->>>>>>> demo-3
 	}
 }
